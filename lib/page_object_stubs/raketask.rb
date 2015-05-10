b505c3738d9013bee4be6ad5c753358d2e52284f
bootstraponline <code@bootstraponline.com>
2015-05-10 14:55:54 -0400

    Update rake task

--- lib/page_object_stubs/raketask.rb
+++ lib/page_object_stubs/raketask.rb
@@ -7,7 +7,7 @@ module PageObjectStubs
     # @option opts [String] :task_name the name of the stubs task (optional)
     # @option opts [String] :task_desc the description of the stubs task (optional)
     # @option opts [lambda] :targets lambda that will return an array of targets (optional)
-    # @option opts [lambda] :targets lambda that will return the output folder (optional)
+    # @option opts [lambda] :output_folder lambda that will return the output folder (optional)
     # @option opts [Boolean] :angularjs generate angularjs stubs. default true (optional)
     def add_stubs_task opts={}
       task_name     = opts.fetch(:task_name, 'stubs')
@@ -22,7 +22,7 @@ module PageObjectStubs
       raise 'output_folder must be a lambda' unless output_folder.lambda?
 
       Rake.application.last_description = task_desc
-      Rake::Task.define_task task_name do |task|
+      Rake::Task.define_task task_name do
         PageObjectStubs.generate targets:       targets.call,
                                  output_folder: output_folder.call,
                                  angularjs:     angularjs
