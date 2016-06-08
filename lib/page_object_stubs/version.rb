96bed9636a3e66679cdf3326d59395cf873a6670
bootstraponline <code@bootstraponline.com>
2016-06-08 10:07:31 -0400

    Release 2.0.1

--- lib/page_object_stubs/version.rb
+++ lib/page_object_stubs/version.rb
@@ -1,4 +1,4 @@
 module PageObjectStubs
-  VERSION = '2.0.0' unless defined? ::PageObjectStubs::VERSION
-  DATE    = '2015-09-24' unless defined? ::PageObjectStubs::DATE
+  VERSION = '2.0.1' unless defined? ::PageObjectStubs::VERSION
+  DATE    = '2016-06-08' unless defined? ::PageObjectStubs::DATE
 end
